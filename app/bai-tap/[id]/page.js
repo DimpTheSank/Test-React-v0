69ff02fbbaeb21b36eaf5f700e4793a6aa83d241
DimpTheSank <dangnlh.auto@gmail.com>
2026-05-13 10:11:40 +0700

    read excel v2

--- app/bai-tap/[id]/page.js
+++ app/bai-tap/[id]/page.js
@@ -4,7 +4,7 @@ import { useRouter } from 'next/navigation'
 import Cookies from 'js-cookie'
 import { db } from '@/lib/firebase'
 import { doc, getDoc } from 'firebase/firestore'
-import { readExcelFromDrive } from '@/lib/readExcel'
+import { readExcelFromDrive } from '@/lib/readexcel'
 
 export default function BaiTap({ params }) {
   const router = useRouter()

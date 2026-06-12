6900b86a5a96a5fc5dc88ddef1da1cfe6b7784b2
DimpTheSank <dangnlh.auto@gmail.com>
2026-06-12 14:33:12 +0700

    new speed for vocab listening

--- app/vocab/[id]/page.js
+++ app/vocab/[id]/page.js
@@ -396,7 +396,7 @@ function ListeningRow({ word, index, isReview, userAnswer, onChange }) {
     if (!window.speechSynthesis) return
     window.speechSynthesis.cancel()
     const utter = new SpeechSynthesisUtterance(correct)
-    utter.lang = 'en-GB'; utter.rate = 0.6
+    utter.lang = 'en-GB'; utter.rate = 0.65
     utter.onstart = () => setSpeaking(true)
     utter.onend = () => setSpeaking(false)
     utter.onerror = () => setSpeaking(false)
